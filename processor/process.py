7fe2b1dfc46799b435a65fa61b5f8f6fd0215cac
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-02-17 18:56:03 -0500

    load values from environment variables

--- processor/process.py
+++ processor/process.py
@@ -1,13 +1,27 @@
+import dotenv
+import os
+from os.path import join, dirname
+
 import agents
 from router import simplerouter
 
 if __name__ == "__main__":
+
+    #Loading the environment variables
+    dotenv_path = join(dirname(__file__), '../conf/.env')
+    dotenv.load_dotenv(dotenv_path)
+
+    #Getting the environment configs
+    MODEL_PATH = os.environ.get("MODEL_PATH")
+    EMBEDDING = os.environ.get("EMBEDDING")
+    DOC_PATH = os.environ.get("DOC_PATH")
+    PROMPT_YAML_PATH = os.environ.get("PROMPT_YAML_PATH")
      
     router = simplerouter.SimpleRouter()
     agent_list = agents.get_agent_list()
     agent = router.select_agent(agent_list)(
-        "models/nous-hermes-llama-2-7b.q4_0.gguf",
-        "sentence-transformers/all-MiniLM-L6-v2",
-        "docs",
-        "conf/prompts.yaml")
+        MODEL_PATH,
+        EMBEDDING,
+        DOC_PATH,
+        PROMPT_YAML_PATH)
     print(agent.perform("What does the print_argument annotation do?"))
