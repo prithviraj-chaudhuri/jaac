3afdd818dde132c10869b4e35128e477f1c8f8a6
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-02-18 13:16:38 -0500

    Changed extension name to "jaac"; Added test auto complete

--- extension.js
+++ extension.js
@@ -3,8 +3,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -12,36 +12,37 @@ const vscode = require('vscode');
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "jaac-test" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('jaac-test.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSCode!');
-	});
-
-	context.subscriptions.push(disposable);
+	console.log('Congratulations, your extension "jaac" is now active!');
 
 	// Create a new disposable for creating a README File
-	let newDisposable = vscode.commands.registerCommand('jaac-test.writeReadme', function (){
-		const wsedit = new vscode.WorkspaceEdit();
-		const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
-		const filePath = vscode.Uri.file(wsPath + '/README2.md');
-		const writeStr = '# Hello World!';
-		const writeData = Buffer.from(writeStr, 'utf8');
-		vscode.window.showInformationMessage(filePath.toString());
-		wsedit.createFile(filePath, { ignoreIfExists: true });
-		vscode.workspace.fs.writeFile(filePath, writeData);
-		vscode.workspace.applyEdit(wsedit);
-		vscode.window.showInformationMessage('Created a new file: README2.md');
+	// let newDisposable = vscode.commands.registerCommand('jaac.writeReadme', function (){
+	// 	const wsedit = new vscode.WorkspaceEdit();
+	// 	const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
+	// 	const filePath = vscode.Uri.file(wsPath + '/README2.md');
+	// 	const writeStr = '# Hello World!';
+	// 	const writeData = Buffer.from(writeStr, 'utf8');
+	// 	vscode.window.showInformationMessage(filePath.toString());
+	// 	wsedit.createFile(filePath, { ignoreIfExists: true });
+	// 	vscode.workspace.fs.writeFile(filePath, writeData);
+	// 	vscode.workspace.applyEdit(wsedit);
+	// 	vscode.window.showInformationMessage('Created a new file: README2.md');
+	// });
+
+	const md = { scheme: 'file', language: 'markdown' };
+	const md_provider = vscode.languages.registerCompletionItemProvider(md, {
+
+		async provideCompletionItems(document, position, token, context) {
+			let title = "Sample"
+
+			const snippetCompletion = new vscode.CompletionItem('Title');
+			snippetCompletion.insertText = new vscode.SnippetString("This is a test string");
+			return [
+				snippetCompletion
+			];
+		}
 	});
 
-	context.subscriptions.push(newDisposable);
+	context.subscriptions.push(md_provider);
 
 }
 
