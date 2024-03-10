ee8ec65f19be36d125a4958126284bc1011294e0
Taylor Agarwal <taylor.agarwal@gmail.com>
2024-03-09 21:30:16 -0500

    ✨ Add support for multiple languages

--- processor/datastore/chroma_store.py
+++ processor/datastore/chroma_store.py
@@ -1,9 +1,19 @@
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain_community.document_loaders.generic import GenericLoader
-from langchain_community.document_loaders.parsers import LanguageParser
+from langchain.document_loaders.directory import DirectoryLoader
+from langchain.document_loaders.text import TextLoader
 from langchain.text_splitter import Language
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores.chroma import Chroma
+
+
+EXTENSION2LANGUAGE = {
+    "py": Language.PYTHON,
+    "js": Language.JS,
+    "html": Language.HTML,
+    "php": Language.PHP,
+    # Add others...
+}
+
 
 class ChromaStore:
 
@@ -12,20 +22,37 @@ class ChromaStore:
             model_name=embedding_model_name,
             model_kwargs={'device': 'cpu'}
         )
-        self.loader = GenericLoader.from_filesystem(
-            doc_path,
+        loader_kwargs = {"autodetect_encoding": True}
+        self.loader = DirectoryLoader(
+            path=doc_path,
             glob="**/*",
+            show_progress=True,
+            loader_cls=TextLoader,
+            loader_kwargs=loader_kwargs,
             exclude=["**/non-utf8-encoding.py"],
-            parser=LanguageParser(parser_threshold=500)
         )
         self.db = Chroma(persist_directory=db_path)
         
     def updateStore(self):
         documents = self.loader.load()
-        splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-        texts = splitter.split_documents(documents)
+        extension2docs = {}
+        for doc in documents:
+            extension = doc.metadata["source"].split(".")[-1]
+            doc.metadata["extension"] = extension
+            if extension not in extension2docs:
+                extension2docs[extension] = []
+            extension2docs[extension].append(doc)
+        split_texts = []
+        for extension, docs in extension2docs.items():
+            language = EXTENSION2LANGUAGE.get(extension, None)
+            if language is None:
+                splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+            else:
+                splitter = RecursiveCharacterTextSplitter.from_language(language=language, chunk_size=2000, chunk_overlap=200)
+            texts = splitter.split_documents(docs)
+            split_texts.extend(texts)
         try:
-            self.db.from_documents(texts, self.embeddings)
+            self.db.from_documents(split_texts, self.embeddings)
             return True
         except Exception:
             return False
